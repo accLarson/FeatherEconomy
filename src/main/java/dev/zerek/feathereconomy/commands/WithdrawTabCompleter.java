d0c9cf29214198e6f885f3c7e9fb102cac340b5b
accLarson <acc.larson@gmail.com>
2023-03-31 10:26:30 -0400

    update to WithdrawTabCompleter

--- src/main/java/dev/zerek/feathereconomy/commands/WithdrawTabCompleter.java
+++ src/main/java/dev/zerek/feathereconomy/commands/WithdrawTabCompleter.java
@@ -1,9 +1,12 @@
 package dev.zerek.feathereconomy.commands;
 
 import dev.zerek.feathereconomy.FeatherEconomy;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,8 +24,33 @@ public class WithdrawTabCompleter implements TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
 
-        if (args.length == 1) return new ArrayList<>();
+        // /withdraw [amount]
+        if (args.length == 1) {
 
-        else return null;
+            // Check if sender has permission
+            if (!sender.hasPermission("feather.economy.withdraw")) return new ArrayList<>();
+
+            ArrayList<String> suggestions = new ArrayList<>();
+
+            if (sender instanceof Player) {
+
+                Player player = (Player) sender;
+
+                suggestions.add(String.valueOf(plugin.getEconomy().getBalance(player)));
+            }
+
+            return suggestions;
+        }
+
+        // /withdraw [amount] [player]
+        else if (args.length == 2){
+
+            // Check if sender has permission
+            if (!sender.hasPermission("feather.economy.withdraw.other")) return new ArrayList<>();
+
+            return null;
+        }
+
+        return new ArrayList<>();
     }
 }
